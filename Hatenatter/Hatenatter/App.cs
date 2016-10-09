544d5e41b6d5a256c094177915a6063aa950f509
kobake <kobake@users.sourceforge.net>
2016-10-09 18:36:45 +0900

    ページ枠組み

--- Hatenatter/Hatenatter/App.cs
+++ Hatenatter/Hatenatter/App.cs
@@ -11,23 +11,7 @@ namespace Hatenatter
     {
         public App()
         {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "Hatenatter",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-
-            MainPage = new NavigationPage(content);
+            MainPage = new Page1();
         }
 
         protected override void OnStart()
