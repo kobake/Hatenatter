6e74d9c629577a82b4889cd87812b4ee92d772a1
kobake <kobake@users.sourceforge.net>
2016-10-13 00:43:10 +0900

    AuthButton

--- Hatenatter/Hatenatter/Page1.xaml.cs
+++ Hatenatter/Hatenatter/Page1.xaml.cs
@@ -11,18 +11,7 @@ namespace Hatenatter
     public partial class Page1 : ContentPage
     {
         Random m_random = new Random();
-
-        // 指定ワードをランダム回繰り返した文字列を作る
-        private string RandomDup(string word)
-        {
-            string ret = word;
-            int n = m_random.Next() % 10;
-            for (int i = 0; i < n; i++)
-            {
-                ret += "\n" + word;
-            }
-            return ret;
-        }
+        
 
         public Page1()
         {
@@ -35,7 +24,7 @@ namespace Hatenatter
                 {
                     Image = "https://avatars0.githubusercontent.com/u/20608487?v=3&s=200",
                     Name = "John",
-                    State = RandomDup("Hello")
+                    State = "Hello"
                 }
             );
             list.Add(
@@ -43,7 +32,7 @@ namespace Hatenatter
                 {
                     Image = "https://avatars0.githubusercontent.com/u/20608487?v=3&s=200",
                     Name = "Kei",
-                    State = RandomDup("こんにちは")
+                    State = "Hello"
                 }
             );
             list.Add(
@@ -51,12 +40,17 @@ namespace Hatenatter
                 {
                     Image = "https://avatars0.githubusercontent.com/u/20608487?v=3&s=200",
                     Name = "Tama",
-                    State = RandomDup("(◔౪◔)!!!")
+                    State = "Hello"
                 }
             );
 
             // Bind
             this.BindingContext = list;
+        }
+
+        private void AuthButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Message", "OK");
         }
     }
 }
