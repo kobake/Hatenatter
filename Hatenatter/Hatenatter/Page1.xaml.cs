85645c7c07bad7bf8005d62c4884ea891277c4a6
kobake <kobake@users.sourceforge.net>
2016-10-13 00:57:54 +0900

    async通信サンプル

--- Hatenatter/Hatenatter/Page1.xaml.cs
+++ Hatenatter/Hatenatter/Page1.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace Hatenatter
@@ -48,9 +48,34 @@ namespace Hatenatter
             this.BindingContext = list;
         }
 
-        private void AuthButton_Clicked(object sender, EventArgs e)
+        private async void AuthButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Title", "Message", "OK");
+            // DisplayAlert("Title", "Message", "OK");
+            // OAuth
+            int result = await StartAuth();
+            await DisplayAlert("Title", "result=" + result, "OK");
+        }
+
+        private async Task<int> StartAuth()
+        {
+            
+            var httpClient = new HttpClient(); // Xamarin supports HttpClient!
+
+            Task<string> contentsTask = httpClient.GetStringAsync("http://xamarin.com"); // async method!
+
+            // await! control returns to the caller and the task continues to run on another thread
+            string contents = await contentsTask;
+
+            TestLabel.Text += "DownloadHomepage method continues after async call. . . . .\n";
+
+            // After contentTask completes, you can calculate the length of the string.
+            int exampleInt = contents.Length;
+
+            TestLabel.Text += "Downloaded the html and found out the length.\n\n\n";
+
+            TestLabel.Text += contents; // just dump the entire HTML
+
+            return exampleInt; // Task<TResult> returns an object of type TResult, in this case int
         }
     }
 }
